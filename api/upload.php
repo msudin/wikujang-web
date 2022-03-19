9ff0975f0a3c947295b0aed4b679345df83e811c
msudin <udin0141@gmail.com>
2022-03-19 08:18:32 +0700

    update api upload

--- api/upload.php
+++ api/upload.php
@@ -1,72 +1,70 @@
-<?php 
+<?php
 include_once('../helper/import.php');
 
 try {
     if(requestMethod() == "POST") {
-        $dToken = validateToken(headerToken()); 
+        $dToken = validateToken(headerToken());
         if ($dToken != NULL) {
-            if(isset($_FILES["image"]) && $_FILES["image"]["error"] == 0 && isset($_POST["type"])) { 
-                $allowed = array("jpg" => "image/jpg", "jpeg" => "image/jpeg", "gif" => "image/gif", "png" => "image/png");
-                $filename = $_FILES["image"]["name"];
-                $filetype = $_FILES["image"]["type"];
-                $filesize = $_FILES["image"]["size"]; 
-                $filetemp = $_FILES["image"]["tmp_name"];
-            
-                // verify file extension
-                $ext = pathinfo($filename, PATHINFO_EXTENSION);
-            
-                // set max size image uploaded
-                $maxsize = 0.1 * 1024 * 1024;
-                if(!array_key_exists($ext, $allowed)) {
-                    response(400, "Format image not allowed");
-                    return;
-                } else if ($filesize > $maxsize) {
-                    response(400, "Size image to large");
-                    return;
-                } else if ($_POST["type"] == "profile") {
-                    // image env
-                    $server_url = urlPathImage(); 
-                    $target_dir = "../uploads/";
-                    $target_file = $target_dir . basename($filename);
-            
-                    /* create new name file */
-                    $newfilename   = uniqid() . "-" . time();
-                    $extension  = pathinfo($filename, PATHINFO_EXTENSION );
-                    $basename   = $newfilename . "." . $extension;
-                    $new_target_file = $target_dir . basename($basename);
-            
-                    if (move_uploaded_file($filetemp, $new_target_file)) {
-                        $dFile = createFile($dToken->userId, $_POST["type"], $basename);
-                        if ($dFile != NULL) {
-                            $breqProfile = new stdClass();
-                            $breqProfile->userId = $dToken->userId;
-                            $breqProfile->fileId = $dFile->id;
-                            $dUpdateProfile = updateUserPhotoProfile($breqProfile);
-                            if ($dUpdateProfile) {
+            if(isset($_FILES["image"]) && $_FILES["image"]["error"] == 0 && isset($_POST["type"])) {
+                $typeImage = $_POST["type"];
+                if ($typeImage == "profile" || $typeImage == "product" || $typeImage == "warung") {
+                    $allowed = array("jpg" => "image/jpg", "jpeg" => "image/jpeg", "gif" => "image/gif", "png" => "image/png");
+                    $filename = $_FILES["image"]["name"];
+                    $filetype = $_FILES["image"]["type"];
+                    $filesize = $_FILES["image"]["size"];
+                    $filetemp = $_FILES["image"]["tmp_name"];
+
+                    // verify file extension
+                    $ext = pathinfo($filename, PATHINFO_EXTENSION);
+
+                    // set max size image uploaded
+                    $maxsize = 0.1 * 1024 * 1024;
+                    if(!array_key_exists($ext, $allowed)) {
+                        response(400, "Format image not allowed");
+                        return;
+                    } else if ($filesize > $maxsize) {
+                        response(400, "Size image to large");
+                        return;
+                    } else {
+                        // image env
+                        $server_url = urlPathImage();
+                        $target_dir = "../uploads/";
+                        $target_file = $target_dir . basename($filename);
+
+                        /* create new name file */
+                        $newfilename   = uniqid() . "-" . time();
+                        $extension  = pathinfo($filename, PATHINFO_EXTENSION );
+                        $basename   = $newfilename . "." . $extension;
+                        $new_target_file = $target_dir . basename($basename);
+
+                        if (move_uploaded_file($filetemp, $new_target_file)) {
+                            $dFile = createFile($dToken->userId, $_POST["type"], $basename);
+                            if ($dFile != NULL) {
                                 $result = array(
                                     "id" => $dFile->id,
-                                    "imageUrl" => $server_url."".$basename,
-                                    "imageName" => $basename, 
+                                    "imageUrl" => urlPathImage()."".$basename,
+                                    "imageName" => $basename,
                                     "type" => $dFile->type,
                                     "createdAt" => $dFile->createdAt
                                 );
                                 response(200, "Success upload image", $result);
                             }
+                            return;
                         }
+                        response(500, "1");
                         return;
-                    } 
-                    response(500);
+                    }
                     return;
-                } 
-                response(500);
-                return;
+                } else {
+                    response(500, "Type image not allowed");
+                }
             } else {
                 response(400, "File Not Found");
             }
             return;
         }
-    } 
-    response(500); 
+    }
+    response(500);
 } catch (Exception $e) {
     $error = $e->getMessage();
     response(500, "upload image : $error");
