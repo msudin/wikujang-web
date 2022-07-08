edac64b46e45bd5ff86490ca1fbe3aeeb9c6809b
Muhammad Sholehuddin <udin0141@gmail.com>
2022-07-08 23:23:19 +0700

    update import class

--- helper/import.php
+++ helper/import.php
@@ -2,9 +2,7 @@
 include_once('../config/config.php');
 include_once('../helper/http.php');
 
-
 include_once('../usecase/user_uc.php');
 include_once('../usecase/warung_uc.php');
 include_once('../usecase/product_uc.php');
-
 ?>
